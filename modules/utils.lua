509d7aaac27e8fcfb4de80ad8f2cc730e3380635
neveix <i@neveix.ru>
2026-08-23 19:08:52 +0500

    commit old changed

--- modules/utils.lua
+++ modules/utils.lua
@@ -4,24 +4,28 @@ local utils = {}
 
 utils.raycast_max_length = 512
 
--- returns x, y, z
-function utils.player_get_rot_vector(playerid) 
+---Возвращает вектор поворота игрока
+---@param playerid integer id игрока
+---@param normalize boolean Нужно ли нормализовать вектор
+function utils.player_get_rot_vector(playerid, normalize)
     local yaw, pitch, roll = player.get_rot(playerid)
     local yaw = math.rad(yaw + 90)
     local pitch = math.rad(pitch)
 
+    local x = math.cos(yaw) * math.cos(pitch)
     local y = math.sin(pitch)
+    local z = -math.sin(yaw) * math.cos(pitch)
 
-    local x, z = 1, 0
-
-    local nx = ( x * math.cos(yaw) - z * math.sin(yaw) ) * math.cos(pitch)
-    local nz = -( x * math.sin(yaw) + z * math.cos(yaw) ) * math.cos(pitch)
-    x, z = nx, nz
-
+    if normalize then
+        local length = math.sqrt(x * x + y * y + z * z)
+        if length > 0 then
+            return x / length, y / length, z / length
+        end
+    end
     return x, y, z
 end
 
----Проводит луч от игрока в направлении его взгляда 
+---Проводит луч от игрока в направлении его взгляда
 ---@param playerid number id игрока от которого будет проводиться луч
 ---@param src_pos_shift vec3|nil Смещение относительно координат игрока
 ---@return vec3|nil (vec3, false) Целочисленная точка конца луча
@@ -38,10 +42,10 @@ function utils.raycast_from_player(playerid, src_pos_shift)
     local rx, ry, rz = utils.player_get_rot_vector(playerid)
 
     local raycast_result = block.raycast(
-        {x+sx, y+sy, z+sz},
-        {rx, ry, rz},
+        { x + sx, y + sy, z + sz },
+        { rx, ry, rz },
         utils.raycast_max_length,
-        {nil, nil, nil, nil, nil, nil})
+        { nil, nil, nil, nil, nil, nil })
     if raycast_result == nil then
         return nil, true
     end
@@ -50,3 +54,4 @@ function utils.raycast_from_player(playerid, src_pos_shift)
 end
 
 return utils
+
