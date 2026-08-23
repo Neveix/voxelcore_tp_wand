4e272fda916b7246fed8e50624e23dea5b9baf77
neveix <i@neveix.ru>
2026-08-23 19:20:52 +0500

    fix bug with use on block

--- scripts/tp_wand.lua
+++ scripts/tp_wand.lua
@@ -5,21 +5,20 @@ local blocks_check_limit = 256
 local final_pos_x_shift = 0.5
 local final_pos_z_shift = 0.5
 
-function on_use_on_block(x, y, z, pid)
-    while not block.is_replaceable_at(x,y,z) do
+function on_use_on_block(x, y, z, playerid)
+    while not block.is_replaceable_at(x, y, z) do
         y = y + 1
         if y > blocks_check_limit then break end
     end
-    player.set_pos(playerid, 
+    player.set_pos(playerid,
         x + final_pos_x_shift,
-        y + 1,
+        y + 0.9,
         z + final_pos_z_shift)
     return true
 end
 
-
 function on_use(playerid)
-    local coords, err = utils.raycast_from_player(playerid, {0, 0.7, 0})
+    local coords, err = utils.raycast_from_player(playerid, { 0, 0.7, 0 })
     if err then
         return
     end
